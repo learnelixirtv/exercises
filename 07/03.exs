e0f15c511a03dbdc58c9dfdca5784fca5dd9b639
Isak Berglind <info@isakberglind.se>
2019-04-17 20:10:37 +0200

    added required exercise functionality to 07/03

--- 07/03.exs
+++ 07/03.exs
@@ -5,6 +5,8 @@ defmodule StreamExample do
     |> Stream.filter(&String.starts_with?(&1, "d"))
     |> Stream.take_every(2)
     |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.capitalize(&1))
+    |> Enum.sort(&(String.length(&1) > String.length(&2)))
     |> Enum.into([])
   end
 end
